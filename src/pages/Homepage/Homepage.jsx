5f4d840bec879233cad9573ea4cca1dbc3eac3c2
jin <jin200126@gmail.com>
2024-04-16 02:15:07 +0900

    test

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const Homepage = () => {
-  return (
-    <div>
-      <div>Homepage</div>
-      <div>Homepage</div>
-    </div>
-  );
+  return <div>Homepage</div>;
 };
 
 export default Homepage;
